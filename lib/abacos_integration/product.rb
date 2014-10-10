2c8bb3fc75040598ab2964c4cbd03c39119aab95
Washington Luiz <huoxito@gmail.com>
2014-10-10 01:00:08 -0300

    add a couple notes about product mappings

--- lib/abacos_integration/product.rb
+++ lib/abacos_integration/product.rb
@@ -7,9 +7,22 @@ module AbacosIntegration
       @product_payload  = payload[:product] || {}
     end
 
+    # NOTE Dont return child products separately? they'd should be nested under
+    # the master product key as variants
+    #
+    # NOTE What about Marcas and Familia? (Web services related to products)
+    #
+    # NOTE Do we need to fetch prices via another webservice?
     def fetch
       Abacos.products_available.map do |p|
-        { id: p[:codigo_produto], abacos: p }
+        {
+          id: p[:codigo_produto],
+          name: p[:nome_produto],
+          sky: p[:codigo_produto],
+          description: p[:descricao],
+          price: p[:preco_tabela1],
+          abacos: p
+        }
       end
     end
 
