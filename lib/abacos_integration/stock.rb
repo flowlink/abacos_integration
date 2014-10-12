a3f2970c00f55af2221d2d950a7d92ab3b0f98fd
Washington Luiz <huoxito@gmail.com>
2014-10-12 20:26:04 -0300

    Use codigo_produto_abacos as inventories ID
    Because that attribute is usually numbers only while codigo_produto
    might contain values like "1234-1"

--- lib/abacos_integration/stock.rb
+++ lib/abacos_integration/stock.rb
@@ -7,10 +7,13 @@ module AbacosIntegration
       @inventory_payload = payload[:inventory] || {}
     end
 
+    # We're using `codigo_produto_abacos` as the ID key here to facilicate
+    # moving this info to a storefront that requires IDs to be unique numbers
+    # only
     def fetch
       Abacos.stocks_available.map do |s|
         {
-          id: s[:codigo_produto],
+          id: s[:codigo_produto_abacos],
           product_id: s[:codigo_produto],
           quantity: s[:saldo_disponivel],
           location: s[:nome_almoxarifado_origem],
